71180fcf78c4bc23fc2778d951a70c84e6814b2b
Brian Le <blsymph@gmail.com>
2015-09-16 07:22:29 +0000

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -5,17 +5,30 @@ defmodule Trex.Mixfile do
     [ app: :trex,
       escript: [main_module: Trex],
       version: "0.0.1",
-      elixir: "~> 1.0.0",
+      elixir: "~> 1.0",
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
       deps: deps ]
   end
 
+
   # Configuration for the OTP application
+  #
+  # Type `mix help compile.app` for more information
   def application do
     [applications: [:hackney]]
   end
 
-  # Returns the list of dependencies in the format:
-  # { :foobar, "~> 0.1", git: "https://github.com/elixir-lang/foobar.git" }
+
+  # Dependencies can be Hex packages:
+  #
+  #   {:mydep, "~> 0.3.0"}
+  #
+  # Or git/path repositories:
+  #
+  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
+  #
+  # Type `mix help deps` for more examples and options
   defp deps do
     [
       # { :httprot, github: "meh/httprot" },
